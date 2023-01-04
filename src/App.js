12b7304c641a57b1d9734f44813055e5e1c96cc4
Divyesh Shrestha <rashudshrestha12@gmail.com>
2023-01-04 13:14:21 +1100

    my-latest-push

--- src/App.js
+++ src/App.js
@@ -23,7 +23,8 @@ function App() {
               </LinkContainer>
               <Nav className="me-auto">
                 <Link to="/cart" className="nav-link">
-                  Cart{cart.cartItems.length > 0 && (
+                  Cart
+                  {cart.cartItems.length > 0 && (
                     <Badge pill bg="danger">
                       {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
                     </Badge>
